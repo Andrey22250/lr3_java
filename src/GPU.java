323ce989ad0d107145b0b987a66b216645d36834
Andrey22250 <andrchepras@mail.ru>
2023-11-07 02:29:02 +0700

    Написан CPU

--- src/GPU.java
+++ src/GPU.java
@@ -1,2 +1,71 @@
+import java.util.Scanner;
 public class GPU {
+    private String name_gpu;
+    private int vram;
+    private int TDP;
+    private int min_TDP;
+    private int flag = 0;
+    public GPU() { flag = 1; }
+    public GPU(String name_gpu) {
+        this.name_gpu = name_gpu;
+        flag = 1;
+    }
+    public GPU(String name_gpu, int vram, int TDP) {
+        SetCPU(name_gpu, vram, TDP);
+        flag = 1;
+    }
+    private boolean CheckCor(String name_gpu, int vram, int TDP)
+    {
+        return vram > 0 && TDP > 0  && !name_gpu.isEmpty();
+    }
+    private void SetCPU(String name_gpu, int vram, int TDP)
+    {
+        if (CheckCor(name_gpu, vram, TDP))
+        {
+            this.name_gpu = name_gpu;
+            this.vram = vram;
+            this.TDP = TDP;
+
+        }
+        if (flag == 0)
+        {
+            this.min_TDP = TDP * 85 / 100;
+            flag = 1;
+        }
+        else
+        {
+            System.out.println("Ошибка ввода данных!");
+            System.exit(-1);
+        }
+    }
+    public String GetName_gpu() {
+        return name_gpu;
+    }
+    public int GetVram() {
+        return vram;
+    }
+    public int GetTDP() {
+        return TDP;
+    }
+    public int GetMin_TDP() {
+        return min_TDP;
+    }
+    public void input_GPU()
+    {
+        {
+            String name_gpu;
+            int vram, TDP;
+
+            Scanner in = new Scanner(System.in);
+
+            System.out.print("Введите название видеокарты: ");
+            name_gpu = in.nextLine();
+            System.out.print("Введите объём видеопамяти в МБ: ");
+            vram = in.nextInt();
+            System.out.print("Введите TDP в ваттах: ");
+            TDP = in.nextInt();
+
+            SetCPU(name_gpu, vram, TDP);
+        }
+    }
 }
