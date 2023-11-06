d2477d2690c456a696c5a677dc10abf5a8303ca0
Andrey22250 <andrchepras@mail.ru>
2023-11-06 21:50:58 +0700

    Написан Motherboard

--- src/Motherboard.java
+++ src/Motherboard.java
@@ -1,2 +1,48 @@
+import java.util.Scanner;
 public class Motherboard {
+    private String name_mrbrd;
+    private String chipset;
+    public Motherboard() { }
+    public Motherboard(String name_mrbrd) {
+        this.name_mrbrd = name_mrbrd;
+    }
+    public Motherboard(String name_mrbrd, String chipset) {
+        SetMrbrd(name_mrbrd, chipset);
+    }
+    private boolean CheckCor(String name_mrbrd, String chipset)
+    {
+        return !name_mrbrd.isEmpty() && !chipset.isEmpty();
+    }
+    private void SetMrbrd(String name_mrbrd, String chipset)
+    {
+        if (CheckCor(name_mrbrd, chipset))
+        {
+            this.name_mrbrd = name_mrbrd;
+            this.chipset = chipset;
+        }
+        else
+        {
+            System.out.println("Ошибка ввода данных!");
+            System.exit(-1);
+        }
+    }
+    public String GetName_mrbrd() {
+        return name_mrbrd;
+    }
+    public String GetChipset() {
+        return chipset;
+    }
+    public void input_mrbrd()
+    {
+        String name_mrbrd, chipset;
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Введите название процессора: ");
+        name_mrbrd = in.nextLine();
+        System.out.print("Введите частоту процессора: ");
+        chipset = in.nextLine();
+
+        SetMrbrd(name_mrbrd, chipset);
+    }
 }
