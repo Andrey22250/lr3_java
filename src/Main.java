fecc6d684d80ca1ecc9b4dc6a94acc985365178b
Andrey22250 <andrchepras@mail.ru>
2023-11-08 00:13:07 +0700

    Написан main, ver 1.0

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,24 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        //пример работы с Build
+        PC[] pcList = new PC[2];
+
+        pcList[0] = new PC();
+        pcList[0].input_pc();
+        System.out.println();
+        pcList[1] = new PC();
+        pcList[1].input_pc();
+        System.out.println();
+
+        pcList[0].out_PC();
+        pcList[1].out_PC();
+        pcList[0].Undervolt_GPU();
+        pcList[0].out_PC();
+
+        //пример работы с Build
+        Build BuildEx = new Build("Андрей");
+        BuildEx.ChangeStatus(Status.FINISHED);
+        BuildEx.out_build();
     }
 }
